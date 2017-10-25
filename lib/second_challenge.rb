781003185cc5f5a28a052970bffaa5bd48b9b536
Amanda Ackerman <ackerm44@gmail.com>
2017-10-25 00:25:03 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,32 +2,32 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {
-        
+
       }
       :matriarch => {
-        
+
       }
       :hero => {
-        
+
       }
       :hero_friends => {
-        
+
       }
-      }, 
+      },
     :capulet => {
       :partriarch => {
-        
+
       }
       :matriarch => {
-        
+
       }
       :heroine => {
-        
+
       }
       :heroine_friends => {
         
       }
       }}
 
-  
+
 end
